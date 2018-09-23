77b673690039a3cbf15989b3ca08c9828dce32cb
Danil Sorokin <danil.sorokin1995@gmail.com>
2018-09-23 11:35:06 +0300

    AR-1.complete-modules-1-4-from-workshop

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    modules: [
+      path.resolve('./lib'),
+      path.resolve('./node_modules')
+    ]
+  },
+  entry: ['babel-polyfill', 'renderers/dom.js'],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ }
+    ]
+  }
+};
